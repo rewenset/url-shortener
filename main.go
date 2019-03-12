f9fc1066987f074e722adcb43211df6091385654
rewenset <a.zakurenyi@gmail.com>
2019-03-12 12:34:54 +0200

    first version of working url shortener

--- main.go
+++ main.go
@@ -4,9 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"text/template"
 
 	"github.com/gorilla/mux"
 )
+
+var last int
+var urls = make(map[int]string)
 
 func main() {
 	r := mux.NewRouter().StrictSlash(true)
@@ -14,23 +19,52 @@ func main() {
 	r.HandleFunc("/", logger(index))
 	r.HandleFunc("/f/{urlID}", logger(follow))
 
-	log.Println("Starting URL shortener server on port 8000")
-	http.ListenAndServe(":8000", r)
+	log.Println("starting URL shortener server on port 8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
 
 func logger(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		h(w, r)
 		log.Printf("%s %s", r.Method, r.RequestURI)
+		h(w, r)
 	}
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "allow creating shortener urls")
+	t := template.Must(template.ParseFiles("index.html"))
+
+	var data string
+	if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {
+			log.Printf("could not parse form: %v", err)
+		}
+		last++
+		urls[last] = r.PostFormValue("original")
+		data = fmt.Sprint(last)
+	}
+
+	if err := t.Execute(w, data); err != nil {
+		log.Printf("could not execute template: %v", err)
+	}
+
 }
 
 func follow(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	urlID := vars["urlID"]
-	fmt.Fprintf(w, "redirect with 302 or send 404 if unknown for url id: %s", urlID)
+
+	urlID, err := strconv.Atoi(vars["urlID"])
+	if err != nil {
+		log.Printf("could not parse urlID to int: %v", err)
+		fmt.Fprint(w, "some error")
+		return
+	}
+
+	if e, ok := urls[urlID]; ok {
+		log.Printf("redirecting to %s", e)
+		http.Redirect(w, r, e, http.StatusFound)
+	} else {
+		log.Printf("requested urlID - %d - not found", urlID)
+		http.NotFound(w, r)
+	}
 }
